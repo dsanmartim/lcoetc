1c4b47d6b0a27ce28f1c6e8e8e652219bb78c5b0
David Sanmartim <dsanmartim@carnegiescience.edu>
2021-07-07 18:51:03 -0400

    Adding new deployment instructions

---
+++ deploy2astronomers.sh
@@ -0,0 +1,6 @@
+# This should be done only if database is modified... so keep it commented
+# scp -r database dsanmartim@astronomers.lco.cl:~/public_html/lcoetc/
+
+# These are the more general updates
+scp -r html tmp dsanmartim@astronomers.lco.cl:~/public_html/lcoetc/
+scp -r cgi-bin dsanmartim@astronomers.lco.cl:~/public_html/
